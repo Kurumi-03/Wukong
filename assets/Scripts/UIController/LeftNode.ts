771e0ac76547b53f211019459dd0f671c0d20753
Kurumi-03 <2524848761@qq.com>
2025-06-12 21:42:25 +0800

    修复了事件系统产生的问题

--- assets/Scripts/UIController/LeftNode.ts
+++ assets/Scripts/UIController/LeftNode.ts
@@ -5,34 +5,34 @@ import {
     Label,
     Node
 } from 'cc';
-import {EventManager} from '../Manager/EventManager';
-import {ConstManager} from '../Manager/ConstManager';
-const {ccclass, property} = _decorator;
+import { EventManager } from '../Manager/EventManager';
+import { ConstManager } from '../Manager/ConstManager';
+const { ccclass, property } = _decorator;
 
 @ccclass('LeftNode')
 export class LeftNode extends Component {
     @property(Label)
-    buyValue : Label | null = null;
+    buyValue: Label | null = null;
 
     @property(Node)
-    buyBtn : Node | null = null;
+    buyBtn: Node | null = null;
 
     @property(Node)
-    freeCount : Node | null = null;
+    freeCount: Node | null = null;
 
     @property(Label)
-    freeNum : Label | null = null;
+    freeNum: Label | null = null;
 
     @property(Label)
-    freeWild : Label | null = null;
+    freeWild: Label | null = null;
 
     @property(CCInteger)
-    baseBuyValue : number = 0;
+    baseBuyValue: number = 0;
 
     protected onLoad(): void {
-        EventManager.Register("ChangeBuyValue", this.ChangeBuyValue);
+        EventManager.Register("ChangeBuyValue", this.ChangeBuyValue.bind(this));
     }
-    
+
     protected start(): void {
         this.ShowBuyBtn();
         this.ChangeBuyValue(ConstManager.Instance(ConstManager).betArray[0]);
@@ -48,21 +48,21 @@ export class LeftNode extends Component {
         this.freeCount.active = true;
     }
 
-    ChangeBuyValue(value : number) {
+    ChangeBuyValue(value: number) {
         this.buyValue.string = "$" + (
             this.baseBuyValue * value
         ).toString();
     }
 
-    ChangeFreeNum(value : number) {
+    ChangeFreeNum(value: number) {
         this.freeNum.string = value.toString();
     }
 
-    ChangeFreeWild(value : number) {
+    ChangeFreeWild(value: number) {
         this.freeWild.string = "*" + value.toString();
     }
 
     protected onDestroy(): void {
-        EventManager.UnRegister("ChangeBuyValue", this.ChangeBuyValue);
+        EventManager.UnRegister("ChangeBuyValue", this.ChangeBuyValue.bind(this));
     }
 }
